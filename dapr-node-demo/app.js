c790ccf713c7191fd84a2973eddca42cf6e166bf
Aziz Dakani <dakani@sipgate.de>
2023-08-06 14:53:04 +0200

    feat: dockerizes all demo apps and uses java template

--- dapr-node-demo/app.js
+++ dapr-node-demo/app.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Dapr!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Demo app listening on port ${port}`)
 })
--- dapr-python-demo/main.py
+++ dapr-python-demo/main.py
@@ -1,3 +1,5 @@
-
 def main():
-    print("Hello World!")
+    print("Hello Dapr!")
+
+if __name__ == "__main__":
+    main()
